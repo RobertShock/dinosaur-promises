5e5d9d7c007f7c16920de3e65b2f4263c4bb41ef
Robbie Shock <rob.f.shock@gmail.com>
2017-10-10 20:03:24 -0500

    EF6

--- dist/app.js
+++ dist/app.js
@@ -1,181 +1,84 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-var dom = require('./dom');
+const dom = require('./dom');
 
-var dinosaurs = [];
+const dinosaurs = [];
 
-
-//The old way - Pyramid of DOOM
-// var dinoGetter = function(){
-// 	$.ajax("./db/dinosaurs.json").done(function(data1){
-// 		console.log("data1", data1);
-// 		data1.dinosaurs1.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		$.ajax("./db/dinosaurs2.json").done(function(data2){
-// 			console.log("data2", data2);
-// 			data2.dinosaurs2.forEach(function(dino){
-// 				dinosaurs.push(dino);
-// 			});
-// 			$.ajax("./db/dinosaurs3.json").done(function(data3){
-// 				console.log("data3", data3);
-// 				data3.dinosaurs3.forEach(function(dino){
-// 					dinosaurs.push(dino);
-// 				});
-// 				console.log("dinosaurs after foreach", dinosaurs);
-// 			});
-// 		});
-// 	});
-// };
-
-var firstDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs.json').done(function(data1){
+const firstDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs.json').done((data1) => {
 			resolve(data1.dinosaurs1);
-		}).fail(function(error1){
+		}).fail((error1) => {
 			reject(error1);
 		});
 	});	
 };
 
-var secondDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs2.json').done(function(data2){
+const secondDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs2.json').done((data2) => {
 			resolve(data2.dinosaurs2);
-		}).fail(function(error2){
+		}).fail((error2) => {
 			reject(error2);
 		});
 	});	
 };
 
-var thirdDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs3.json').done(function(data3){
+const thirdDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs3.json').done((data3) => {
 			resolve(data3.dinosaurs3);
-		}).fail(function(error3){
+		}).fail((error3) => {
 			reject(error3);
 		});
 	});	
 };
 
-
-//PROMISE WORKS - promise pyramid of DOOM
-// var dinoGetter = function(){
-// 	firstDinosaurJSON().then(function(results){
-// 		results.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		secondDinosaurJSON().then(function(results2){
-// 			results.forEach(function(dino){
-// 				dinosaurs.push(dino);
-// 			});	
-// 		});
-// 		thirdDinosaurJSON().then(function(results3){
-// 			results.forEach(function(dino){
-// 				dinosaurs.push(dino);
-// 			});	
-// 			console.log("dinosaurs", dinosaurs);
-// 		});
-// 	}).catch(function(error){
-// 		console.log("error", error);
-// 	});
-// };
-
-
-//Promise the correct way
-// var dinoGetter = function(){
-// 	firstDinosaurJSON().then(function(results){
-// 		results.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		console.log("dinosaur", dinosaurs);
-// 		return secondDinosaurJSON();
-// 	}).then(function(results2){
-// 		console.log("results2", results2);
-// 		results2.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		return thirdDinosaurJSON();
-// 	}).then(function(results3){
-// 		results3.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		console.log("dinosaurs", dinosaurs);
-// 		makeDinos();
-// 	});
-// };
-
-var dinoGetter = function(){
-	Promise.all([firstDinosaurJSON(), secondDinosaurJSON(), thirdDinosaurJSON()]).then(function(results){
+const dinoGetter = () => {
+	Promise.all([firstDinosaurJSON(), secondDinosaurJSON(), thirdDinosaurJSON()]).then((results) => {
 		console.log("results from promise.all", results);
-		results.forEach(function(result){
-			result.forEach(function(dino){
+		results.forEach((result) => {
+			result.forEach((dino) => {
 				console.log("dino", dino);
 				dinosaurs.push(dino);
 			});
 		});
 		makeDinos();
-	}).catch(function(error){
+	}).catch((error) => {
 		console.log("error from promise.all", error);
 	});
 };
 
-
-
-
-
-
-
-var makeDinos = function(){
-	dinosaurs.forEach(function(dino){
+const makeDinos = () => {
+	dinosaurs.forEach((dino) => {
 		dom(dino);
 	});
 };
 
-
-
-
-
-
-var initializer = function(){
+const initializer = () => {
 	dinoGetter();	
 };
 
-var getDinosaurs = function(){
+const getDinosaurs = () => {
 	return dinosaurs;
 };
 
 module.exports = {initializer:initializer, getDinosaurs:getDinosaurs};
 },{"./dom":2}],2:[function(require,module,exports){
-"use strict";
+"use strict"; 
 
-// const buildDomString = (dinosaur) => {
-// 	let domString = '';
-// 	dinosaur.forEach((dino) => {
-// 		domString += `<h1>${dino.name}</h1>`;
-// 	});
-// 	writeToDom(domString);
-// };
+const outputDiv = $('#dinosaurs');
 
-// const writeToDom = (domString) => {
-// 	$('#dinosaurs').html(domString);
-// };
-
-// module.exports = {buildDomString};
-    
-
-var outputDiv = $('#dinosaurs');
-
-var domString = function(dinosaur) {
-	var domStrang = '';
+const domString = (dinosaur) => {
+	let domStrang = '';
 	 	domStrang += `<div>`;
 	 	domStrang += `<h1>${dinosaur.type}</h1>`;
 	 	domStrang += `</div>`;
 	 printToDom(domStrang);
 };
 
-var printToDom = function(strang) {
+const printToDom = (strang) => {
 	outputDiv.append(strang);
 };
 
@@ -184,13 +87,9 @@ module.exports = domString;
 },{}],3:[function(require,module,exports){
 "use strict";
 
-// const dom = require('./dom');
+const data = require('./data');
 
-// dom.buildDomString([{name: 'T-REX!'}]);
-
-var data = require('./data');
-
-$(document).ready(function() {
+$(document).ready(() => {
 	data.initializer();
 });
 },{"./data":1}]},{},[3]);
